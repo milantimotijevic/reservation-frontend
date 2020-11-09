8e1ec1a99bacad8901d1ddacdb63210742fa8d38
Milan Timotijevic <milantimotijevic@Milans-MacBook-Pro.local>
2020-11-09 12:58:08 +0100

    initial

---
+++ public/index.js
@@ -0,0 +1,25 @@
+const reservationsList = document.getElementById('reservations');
+
+axios.get('http://localhost:3000/reservation').then(function(resp) {
+	const reservations = resp.data;
+	reservations.forEach(reservation => {
+		const li = document.createElement('li');
+		li.appendChild(document.createTextNode(`${reservation.guestName} / ${reservation.hotelName}`));
+		reservationsList.appendChild(li);
+	});
+});
+
+const guest = document.getElementById('guest');
+const hotel = document.getElementById('hotel');
+const createReservation = document.getElementById('create-reservation');
+
+createReservation.addEventListener('click', () => {
+	const reservation = {
+		guestName: guest.value,
+		hotelName: hotel.value,
+		startDate: new Date(),
+		endDate: new Date()
+	};
+
+	axios.post('http://localhost:3000/reservation', reservation);
+});
